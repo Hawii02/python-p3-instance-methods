3a6c482fcb92c74b9b4419944e09d46c017fd01e
leon <onditileon21@gmail.com>
2024-03-02 21:40:56 +0300

    completed

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+
+Tom = Person()
+Tom.talk()
+Tom.walk()
+# Dan = Person()
+# Dan.walk()
+
+
